6e509586b53c4c6600f4f14a178049cf59d828e5
Victor Polyakov <polyakov.victor21@gmail.com>
2023-10-17 16:02:17 +0300

    added output exeption

--- reactfrontend/src/BlogPage.tsx
+++ reactfrontend/src/BlogPage.tsx
@@ -9,18 +9,27 @@ import { Helmet } from 'react-helmet';
 
 
 
-class BlogPage extends React.Component{
-  state = { details: [], }
+class BlogPage extends React.Component {
+  state = { details: [], hasData: true };
 
-
-  componentDidMount(): void {
-      let data;
-      axios.get('http://localhost:8000/api/post/')
-      .then(res => {data = res.data; this.setState({ details: data});})
-      .catch(err => {console.log(err);})
-      console.log(data)
+  componentDidMount() {
+    axios
+      .get('http://localhost:8000/api/post/')
+      .then((res) => {
+        const data = res.data;
+        if (data.length === 0) {
+          this.setState({ hasData: false });
+        } else {
+          this.setState({ details: data });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
+
   render() {
+    const { hasData } = this.state;
     return (
       <div>
         <Helmet>
@@ -29,11 +38,18 @@ class BlogPage extends React.Component{
         <Navbar />
         <div className="album bg-body-tertiary div-content py-5">
           <div className='container'>
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            {!hasData && (
+              <div className="warning-message">
+                <p>No posts available.</p>
+              </div>
+            )}
+            {hasData && (
+              <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                 {this.state.details.map((output, id) => (
                     <Post key={id} product={output} />
                 ))}
-            </div>
+              </div>
+            )}
           </div>
         </div>
         <Footer />
