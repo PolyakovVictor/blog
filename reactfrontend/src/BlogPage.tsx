c3c3791b935fbbb1b9316c5de414fcad09cb32bc
Victor Polyakov <polyakov.victor21@gmail.com>
2023-11-11 18:00:55 +0200

    finished pagination

--- reactfrontend/src/BlogPage.tsx
+++ reactfrontend/src/BlogPage.tsx
@@ -1,39 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import './style/blogpage.css';
 import { Post } from './components/post';
 import Navbar from './components/navbar';
 import "bootstrap/dist/js/bootstrap.min.js";
 import Footer from './components/footer';
+import ReactPaginate from 'react-paginate';
 import { Helmet } from 'react-helmet';
 
-
-
-class BlogPage extends React.Component{
-  state = { details: [], }
-
-
-  componentDidMount(): void {
-      let data;
-      axios.get('http://localhost:8000/api/post/')
-      .then(res => {data = res.data; this.setState({ details: data});})
-      .catch(err => {console.log(err);})
-      console.log(data)
+class BlogPage extends Component {
+  state = {
+    posts: [],
+    currentPage: 1,
+    totalPages: 1,
   }
+
+  fetchPosts = async (page : any) => {
+    try {
+      const response = await axios.get(`http://localhost:8000/api/post/?page=${page}`);
+      this.setState({
+        posts: response.data.results,
+        totalPages: response.data.count,
+      });
+    } catch (error) {
+      console.error('Error loading data:', error);
+    }
+  };
+
+  componentDidMount() {
+    this.fetchPosts(this.state.currentPage);
+  }
+
+  handlePageChange = ({ selected }: { selected : number }) => {
+    const newPage = selected + 1;
+    this.setState({ currentPage: newPage }, () => {
+      this.fetchPosts(this.state.currentPage);
+    });
+  };
+
   render() {
     return (
       <div>
         <Helmet>
           <html lang="en" data-bs-theme="dark"></html>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css"></link>
         </Helmet>
         <Navbar />
         <div className="album bg-body-tertiary div-content py-5">
           <div className='container'>
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                {this.state.details.map((output, id) => (
+                {this.state.posts.map((output, id) => (
                     <Post key={id} product={output} />
                 ))}
             </div>
+            <nav className="pagination justify-content-center py-3">
+              <ReactPaginate
+                pageCount={Math.ceil(this.state.totalPages / 10)}
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={2}
+                onPageChange={this.handlePageChange}
+                containerClassName={'pagination'}
+                activeClassName='page-item active'
+                breakClassName='page-item'
+                nextClassName='page-item'
+                disabledClassName='page-item disabled'
+                pageClassName='page-item'
+                previousClassName='page-item'
+                previousLinkClassName='page-link'
+                activeLinkClassName='page-link'
+                pageLinkClassName='page-link'
+                nextLinkClassName='page-link'
+              />
+            </nav>
           </div>
         </div>
         <Footer />
@@ -41,4 +79,5 @@ class BlogPage extends React.Component{
     );
   }
 }
+
 export default BlogPage;
